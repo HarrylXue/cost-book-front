d6a7dafe989f71fe4680e85fffc0739f298ed851
Harry <svip.harry@gmail.com>
2022-08-10 10:39:15 +0800

    feat: 新增测试

---
+++ test1/main.js
@@ -0,0 +1,14 @@
+/*
+ * @Project: 
+ * @Author: Harry|Xue
+ * @Date: 2022-03-04 10:31:34
+ * @Description: 
+ * @version: 
+ * @LastEditors: Harry|Xue
+ * @LastEditTime: 2022-03-04 10:32:41
+ */
+import uniq from 'lodash-es/uniq'
+
+const arr = [1, 2, 3, 3, 4]
+
+console.log(uniq(arr))
---
+++ test2/src/main.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import './index.css'
+// import App from './App'
+import uniq from 'lodash-es/uniq'
+
+const arr = [1, 2, 3, 3, 4]
+
+console.log(uniq(arr))
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
